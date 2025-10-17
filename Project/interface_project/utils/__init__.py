ec97bade05ea52ed574aa7828b1a07cda925b33c
Vincent Angoulvant <vincent.angoulvant@gmail.com>
2025-10-17 16:20:39 +0200

    remove front and other versions

--- Project/interface_project/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Module utilitaire pour l'application de visualisation
-"""
-
-from .helpers import format_time, format_number, get_timestamp, Timer
-
-__all__ = ['format_time', 'format_number', 'get_timestamp', 'Timer']
--- Project/interface_project/workers/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Module de workers (threads) pour les calculs lourds
-"""
-
-from .graph_builder import GraphBuilderWorker
-from .search_worker import SearchWorker
-
-__all__ = ['GraphBuilderWorker', 'SearchWorker']
